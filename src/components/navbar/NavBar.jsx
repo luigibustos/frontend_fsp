5bc291d032e678e9ab63c0ea04b668367eec46a4
luigibustos <louisalphonsobustos@gmail.com>
2023-01-22 23:30:58 -0800

    Updating Homepage

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,20 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faFileLines } from "@fortawesome/free-solid-svg-icons";
 import { HashLink } from "react-router-hash-link";
 const NavBar = () => {
+  const navItems = [
+    {
+      name: "HOME",
+      route: "/#home",
+    },
+    {
+      name: "ABOUT",
+      route: "/#about",
+    },
+    {
+      name: "PROJECTS",
+      route: "/#projects",
+    },
+  ];
   return (
-    <nav class="h-14 w-full bg-green-200 fixed top-0 left-0 right-0 flex justify-between items-center bg-cultured drop-shadow-md px-8 z-50">
+    <nav class="h-14 w-full bg-green-200 fixed top-0 left-0 right-0 flex justify-between items-center bg-cultured text-eeireBlack drop-shadow-md px-8 z-50">
       <div className="flex flex-row gap-3 text-md">
-        <HashLink smooth to={"/#home"}>
-          HOME
-        </HashLink>
-        <HashLink smooth to={"/#about"}>
-          ABOUT
-        </HashLink>
-        <HashLink smooth to={"/#projects"}>
-          PPROJECTS
-        </HashLink>
+        {navItems.map((item) => {
+          return (
+            <HashLink
+              smooth
+              to={item.route}
+              className="hover:text-orangeWeb ease-linear duration-500"
+            >
+              {item.name}
+            </HashLink>
+          );
+        })}
       </div>
-      <div>
-        <h1>Luigi Bustos</h1>
+      <div className="flex flex-row gap-3 text-xl">
+        <a className="cursor-pointer hover:text-orangeWeb ease-linear duration-500">
+          <FontAwesomeIcon icon={faFileLines} />
+        </a>
+        <a className="cursor-pointer hover:text-orangeWeb hover:opacity-80 ease-linear duration-500">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+        <span>Luigi Bustos</span>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,282 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {} from "@fortawesome/free-solid-svg-icons";
+import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { HashLink } from "react-router-hash-link";
 
 function Home() {
   return (
-    <div className="h-screen w-screen bg-platinum flex items-center justify-center">
-      <div className="relative w-full max-w-lg">
-        <div className="absolute inset-1 text-center z-10">
-          <h1 className="text-spanishGrey">FULL STACK ENGINEER</h1>
-          <h1 className="text-6xl drop-shadow-xlg my-5">LUIGI BUSTOS</h1>
-          <div>
-            <a
-              href="https://www.linkedin.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="mx-6 cursor-pointer border-b-4 border-solid border-spanishGrey hover:border-orangeWeb ease-linear duration-700"
+    <div className="h-screen w-screen bg-platinum relative">
+      <div className="absolute w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          // xmlns:xlink="http://www.w3.org/1999/xlink"
+          // xmlns:svgjs="http://svgjs.com/svgjs"
+          width="100%"
+          height="auto"
+          preserveAspectRatio="none"
+          viewBox="0 0 1440 600"
+        >
+          <g mask='url("#SvgjsMask1567")' fill="none">
+            <rect
+              width="1440"
+              height="560"
+              x="0"
+              y="0"
+              fill="rgba(235, 235, 235, 1)"
+            ></rect>
+            <path
+              d="M1026.04 116.66L1013.3 115.44 1011.69 102.73 998.94 101.51 997.33 88.81 984.59 87.59 982.98 74.88M1031.62 110.92L1018.87 109.7 1017.26 96.99 1004.51 95.77 1002.9 83.07 990.16 81.85 988.55 69.14M1037.19 105.18L1024.44 103.95 1022.83 91.25 1010.08 90.03 1008.47 77.32 995.73 76.1 994.12 63.4"
+              stroke="rgba(251, 164, 35, 1)"
+              stroke-width="2.95"
+              stroke-dasharray="3, 2"
+            ></path>
+            <rect
+              width="297.36"
+              height="297.36"
+              clip-path='url("#SvgjsClipPath1568")'
+              x="-95.47"
+              y="288.24"
+              fill="url(#SvgjsPattern1569)"
+              transform="rotate(75.27, 53.21, 436.92)"
+            ></rect>
+            <rect
+              width="360"
+              height="360"
+              clip-path='url("#SvgjsClipPath1570")'
+              x="1051.01"
+              y="285.55"
+              fill="url(#SvgjsPattern1571)"
+              transform="rotate(226.18, 1231.01, 465.55)"
+            ></rect>
+            <path
+              d="M1515.98 419.65 L1574.4499999999998 459.52L1578.4801950221577 406.3198049778421z"
+              stroke="rgba(251, 164, 35, 1)"
+              stroke-width="1.61"
+            ></path>
+            <rect
+              width="163.79999999999998"
+              height="163.79999999999998"
+              clip-path='url("#SvgjsClipPath1572")'
+              x="369.01"
+              y="246.11"
+              fill="url(#SvgjsPattern1573)"
+              transform="rotate(341.7, 450.91, 328.01)"
+            ></rect>
+            <path
+              d="M598.8 281.73 L699.44 372.32L736.1047237675862 240.04027623241382z"
+              stroke="rgba(105, 116, 124, 1)"
+              stroke-width="1"
+              stroke-dasharray="3, 2"
+            ></path>
+            <path
+              d="M1037.99 633.8599999999999 L1037.85 576.0699999999999L1076.3626394706432 566.5223605293568z"
+              stroke="rgba(251, 164, 35, 1)"
+              stroke-width="2.73"
+              stroke-dasharray="3, 3"
+            ></path>
+            <rect
+              width="108"
+              height="108"
+              clip-path='url("#SvgjsClipPath1574")'
+              x="1026.63"
+              y="426.16"
+              fill="url(#SvgjsPattern1575)"
+              transform="rotate(137.51, 1080.63, 480.16)"
+            ></rect>
+            <rect
+              width="216.96"
+              height="216.96"
+              clip-path='url("#SvgjsClipPath1576")'
+              x="289.83"
+              y="65.56"
+              fill="url(#SvgjsPattern1577)"
+              transform="rotate(91.31, 398.31, 174.04)"
+            ></rect>
+            <path
+              d="M966.91 229.57a5.6 5.6 0 1 0 9.83-5.36 5.6 5.6 0 1 0-9.83 5.36zM980.95 221.9a5.6 5.6 0 1 0 9.83-5.36 5.6 5.6 0 1 0-9.83 5.36zM994.99 214.24a5.6 5.6 0 1 0 9.83-5.37 5.6 5.6 0 1 0-9.83 5.37zM1009.03 206.57a5.6 5.6 0 1 0 9.83-5.37 5.6 5.6 0 1 0-9.83 5.37zM946.49 258.95a5.6 5.6 0 1 0 9.83-5.36 5.6 5.6 0 1 0-9.83 5.36zM960.53 251.28a5.6 5.6 0 1 0 9.83-5.36 5.6 5.6 0 1 0-9.83 5.36zM974.57 243.62a5.6 5.6 0 1 0 9.83-5.37 5.6 5.6 0 1 0-9.83 5.37zM988.62 235.95a5.6 5.6 0 1 0 9.83-5.37 5.6 5.6 0 1 0-9.83 5.37z"
+              fill="rgba(251, 164, 35, 1)"
+            ></path>
+            <rect
+              width="132"
+              height="132"
+              clip-path='url("#SvgjsClipPath1578")'
+              x="516.65"
+              y="301.1"
+              fill="url(#SvgjsPattern1579)"
+              transform="rotate(245.61, 582.65, 367.1)"
+            ></rect>
+            <path
+              d="M974.4 258.46 L1034.67 320.5L957.7692585211648 336.2457414788352z"
+              fill="rgba(105, 116, 124, 1)"
+            ></path>
+          </g>
+          <defs>
+            <mask id="SvgjsMask1567">
+              <rect width="1440" height="560" fill="#ffffff"></rect>
+            </mask>
+            <pattern
+              x="0"
+              y="0"
+              width="7.08"
+              height="7.08"
+              patternUnits="userSpaceOnUse"
+              id="SvgjsPattern1569"
             >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                className="h-8 hover:text-orangeWeb ease-linear duration-500"
-              />
-              <span className="ml-2 text-2xl">LinkedIn</span>
-            </a>
-            <a
-              href="https://github.com/"
-              target="_blank"
-              rel="noreferrer"
-              className="mx-6 cursor-pointer border-b-4 border-solid border-spanishGrey hover:border-orangeWeb ease-linear duration-500"
+              <path
+                d="M3.54 1L3.54 6.08M1 3.54L6.08 3.54"
+                stroke="rgba(251, 164, 35, 1)"
+                fill="none"
+                stroke-width="1"
+              ></path>
+            </pattern>
+            <clipPath id="SvgjsClipPath1568">
+              <circle r="74.34" cx="53.21" cy="436.92"></circle>
+            </clipPath>
+            <pattern
+              x="0"
+              y="0"
+              width="360"
+              height="6"
+              patternUnits="userSpaceOnUse"
+              id="SvgjsPattern1571"
             >
-              <FontAwesomeIcon
-                icon={faGithub}
-                className="h-8 hover:text-orangeWeb ease-linear duration-700"
-              />
-              <span className="ml-2 text-2xl">GitHub</span>
-            </a>
-          </div>
+              <rect
+                width="360"
+                height="3"
+                x="0"
+                y="0"
+                fill="rgba(111, 115, 47, 1)"
+              ></rect>
+              <rect
+                width="360"
+                height="3"
+                x="0"
+                y="3"
+                fill="rgba(0, 0, 0, 0)"
+              ></rect>
+            </pattern>
+            <clipPath id="SvgjsClipPath1570">
+              <circle r="90" cx="1231.01" cy="465.55"></circle>
+            </clipPath>
+            <pattern
+              x="0"
+              y="0"
+              width="9.1"
+              height="9.1"
+              patternUnits="userSpaceOnUse"
+              id="SvgjsPattern1573"
+            >
+              <path
+                d="M0 9.1L4.55 0L9.1 9.1"
+                stroke="rgba(111, 115, 47, 1)"
+                fill="none"
+              ></path>
+            </pattern>
+            <clipPath id="SvgjsClipPath1572">
+              <circle r="40.949999999999996" cx="450.91" cy="328.01"></circle>
+            </clipPath>
+            <pattern
+              x="0"
+              y="0"
+              width="6"
+              height="6"
+              patternUnits="userSpaceOnUse"
+              id="SvgjsPattern1575"
+            >
+              <path
+                d="M3 1L3 5M1 3L5 3"
+                stroke="rgba(111, 115, 47, 1)"
+                fill="none"
+                stroke-width="1"
+              ></path>
+            </pattern>
+            <clipPath id="SvgjsClipPath1574">
+              <circle r="27" cx="1080.63" cy="480.16"></circle>
+            </clipPath>
+            <pattern
+              x="0"
+              y="0"
+              width="216.96"
+              height="13.56"
+              patternUnits="userSpaceOnUse"
+              id="SvgjsPattern1577"
+            >
+              <rect
+                width="216.96"
+                height="6.78"
+                x="0"
+                y="0"
+                fill="rgba(105, 116, 124, 1)"
+              ></rect>
+              <rect
+                width="216.96"
+                height="6.78"
+                x="0"
+                y="6.78"
+                fill="rgba(0, 0, 0, 0)"
+              ></rect>
+            </pattern>
+            <clipPath id="SvgjsClipPath1576">
+              <circle r="54.24" cx="398.31" cy="174.04"></circle>
+            </clipPath>
+            <pattern
+              x="0"
+              y="0"
+              width="6"
+              height="6"
+              patternUnits="userSpaceOnUse"
+              id="SvgjsPattern1579"
+            >
+              <path
+                d="M0 6L3 0L6 6"
+                stroke="rgba(251, 164, 35, 1)"
+                fill="none"
+              ></path>
+            </pattern>
+            <clipPath id="SvgjsClipPath1578">
+              <circle r="33" cx="582.65" cy="367.1"></circle>
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 text-eeireBlack drop-shadow-xl">
+        <h1 className="text-spanishGrey text-2xl">FULL STACK ENGINEER</h1>
+        <h1 className="text-8xl my-5">LUIGI BUSTOS</h1>
+        <div>
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="mx-6 cursor-pointer border-b-4 border-solid border-spanishGrey hover:border-orangeWeb ease-linear duration-700"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="h-8 hover:text-orangeWeb ease-linear duration-500"
+            />
+            <span className="ml-2 text-3xl">LinkedIn</span>
+          </a>
+          <a
+            href="https://github.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="mx-6 cursor-pointer border-b-4 border-solid border-spanishGrey hover:border-orangeWeb ease-linear duration-500"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="h-8 hover:text-orangeWeb ease-linear duration-700"
+            />
+            <span className="ml-2 text-2xl">GitHub</span>
+          </a>
         </div>
+      </div>
+      <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-bounce">
+        <HashLink smooth to={"/#about"} className="">
+          <FontAwesomeIcon
+            className="h-12 text-spanishGrey hover:text-orangeWeb duration-500 ease-linear"
+            icon={faAngleDoubleDown}
+          />
+        </HashLink>
       </div>
     </div>
   );
